16cdd5c2b9315e355b32fc524dbfb5a70c592b93
Farooq Abdulla <farooqabdulla279@gmail.com>
2024-05-13 01:43:28 -0500

    added S3 URL

--- components/ui/videoComponent.tsx
+++ components/ui/videoComponent.tsx
@@ -4,7 +4,7 @@ const VideoComponent = () => {
     return (
         <div className=' w-[100%]  mx-auto relative  overflow-auto'>
             <video className='w-full' loop muted autoPlay>
-                <source src='/video.mp4' />
+                <source src='https://hitch-video.s3.us-east-2.amazonaws.com/video.mp4' />
             </video>
             <BgVideoBox />
         </div>
